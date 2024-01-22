6cf34114b26f4d0d56561c6393a14eee983df748
shaxzodaliyorov <shaxzodaliyorov0202@gmail.com>
2024-01-22 14:31:23 +0300

    comment

--- index.js
+++ index.js
@@ -39,8 +39,6 @@ bot.on("contact", async (msg) => {
   const { phone_number, first_name, user_id } = msg.contact;
   const phoneNumber = msg.contact.phone_number;
 
-  console.log(chatId);
-
   const keyboardButtons = () => {
     const keyboards = [
       ["Mahsulotlar"],
@@ -72,5 +70,61 @@ bot.on("contact", async (msg) => {
 
   bot.sendMessage(chatId, `Telefon raqamingiz: ${phoneNumber}`, {
     reply_markup: replyMarkup,
+  });
+});
+
+bot.onText(/Admin Panel/, async (msg) => {
+  const chatId = msg.chat.id;
+  const keyboardButtons = () => {
+    const buttons = [
+      ["Mahsulot Qo'shish", "Barcha Mahsulotlar"],
+      ["Adminlar", "Admin Qo'shish"],
+    ];
+
+    if (chatId === superAdminID) {
+      return buttons;
+    }
+
+    return [];
+  };
+
+  const replyMarkup = {
+    keyboard: keyboardButtons(),
+    resize_keyboard: true,
+    one_time_keyboard: true,
+  };
+
+  const replayMessage =
+    chatId === superAdminID
+      ? "Super Admin Paneliga Xush kelibsiz"
+      : "Siz Admin Emasiz";
+
+  bot.sendMessage(chatId, replayMessage, {
+    reply_markup: replyMarkup,
+  });
+});
+
+bot.onText(/Mahsulot Qo'shish/, async (msg) => {
+  const chatId = msg.chat.id;
+  // Foydalanuvchi yuborgan rasm so'roqni jo'natish
+  //   const keyboard = {
+  //     reply_markup: {
+  //       one_time_keyboard: true,
+  //       keyboard: [[{ text: "Rasmni yuborish", request_contact: true }]],
+  //     },
+  //   };
+
+  bot.sendMessage(chatId, "Rasmni yuboring:");
+
+  bot.on("photo", (msg) => {
+    const chatId = msg.chat.id;
+    const photoId = msg.photo[msg.photo.length - 1].file_id;
+    bot.sendMessage(chatId, "Rahmat! Rasmni qabul qildim", {
+      reply_to_message_id: msg.message_id,
+    });
+    bot.sendMessage(chatId, "Endi Mahsulot Nomini kiriting:");
+    bot.on("text", (ms) => {
+      console.log(ms);
+    });
   });
 });
